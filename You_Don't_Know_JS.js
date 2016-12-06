2b4de51afea4eb296a87c6d365117aa74fb4ba79
nminhduc <nminhduc@users.noreply.github.com>
2016-12-06 15:28:58 +0700

    Update You_Don't_Know_JS.js

--- You_Don't_Know_JS.js
+++ You_Don't_Know_JS.js
@@ -9,3 +9,59 @@ IIFE là 1 pattern rất phổ biến trong Javascript. Pattern này đơn giả
 })();
  
 console.log( foo ); // undefined!
+
+Clousre: ghi nho function 
+function makeAdder(x) {
+    // parameter `x` is an inner variable
+
+    // inner function `add()` uses `x`, so
+    // it has a "closure" over it
+    function add(y) {
+        return y + x;
+    };
+
+    return add;
+}
+var plusOne = makeAdder( 1 );
+
+// `plusTen` gets a reference to the inner `add(..)`
+// function with closure over the `x` parameter of
+// the outer `makeAdder(..)`
+var plusTen = makeAdder( 10 );
+
+plusOne( 3 );       // 4  <-- 1 + 3
+plusOne( 41 );      // 42 <-- 1 + 41
+
+plusTen( 13 );      // 23 <-- 10 + 13
+
+Javascript Module
+
+Eval - so bad :
+function foo(str, a) {
+    eval( str ); // cheating!
+    console.log( a, b );
+}
+
+var b = 2;
+
+foo( "var b = 3;", 1 ); // 1, 3
+lexical scope
+
+With in Javascript:
+var obj = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+
+// more "tedious" to repeat "obj"
+obj.a = 2;
+obj.b = 3;
+obj.c = 4;
+
+// "easier" short-hand
+with (obj) {
+    a = 3;
+    b = 4;
+    c = 5;
+}
